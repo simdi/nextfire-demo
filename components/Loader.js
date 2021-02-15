34fa6c598e871b789827d0811bc9bf3936ee4405
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2021-02-15 16:12:24 +0100

    Added loader component.

---
+++ components/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Loader({ show }) {
+  return show ? (
+    <div className="loader"></div>
+  ) : null;
+}
+
+export default Loader
---
+++ pages/sign-up.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function signUp() {
+  return (
+    <div>
+      Sign up page
+    </div>
+  )
+}
+
+export default signUp;
